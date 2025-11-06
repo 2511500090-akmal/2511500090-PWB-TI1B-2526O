d3beefdf633156a6a2969cc4d3f89d1e2731d4c3
Akmal Fadhillah Dwiputra <2511500090@mahasiswa.atmaluhur.ac.id>
2025-11-06 11:08:38 +0700

    menambahkan 5x5 variabel mata kuliah

--- pertemuan-06/index.php
+++ pertemuan-06/index.php
@@ -57,6 +57,44 @@
             <p><strong>Nama Adek:</strong>$nama_adek</p>
         </section>
 
+        </section id="ipk">
+         <?php
+        $namaMatkul1= "";
+        $namaMatkul2= "";
+        $namaMatkul3= "";
+        $namaMatkul4= ""; 
+        $namaMatkul5= "";
+
+$sksMatkul1= "";
+$sksMatkul2= "";
+$sksMatkul3= "";
+$sksMatkul4= "";
+$sksMatkul5= "";
+
+$nilaiHadir1= ""; 
+$nilaiHadir2= ""; 
+$nilaiHadir3= ""; 
+$nilaiHadir4= "";
+$nilaiHadir5= "";
+
+$nilaiTugas1= ""; 
+$nilaiTugas2= ""; 
+$nilaiTugas3= ""; 
+$nilaiTugas4= ""; 
+$nilaiTugas5= "";
+
+$nilaiUTS1= ""; 
+$nilaiUTS2= ""; 
+$nilaiUTS3= ""; 
+$nilaiUTS4= ""; 
+$nilaiUTS5= "";
+
+$nilaiUAS1= ""; 
+$nilaiUAS2= ""; 
+$nilaiUAS3= ""; 
+$nilaiUAS4= ""; 
+$nilaiUAS5= "";
+
         <section id="contact">
             <h2>Kontak Kami</h2>
             <form action="#" method="GET">
