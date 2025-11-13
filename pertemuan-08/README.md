19911303a9526705a1a7c90bd1561de3159f06d7
Akmal Fadhillah Dwiputra <2511500090@mahasiswa.atmaluhur.ac.id>
2025-11-13 11:22:40 +0700

    menambahkan session start

--- pertemuan-08/README.md
+++ pertemuan-08/README.md
@@ -13,4 +13,5 @@ Proses saya mengerjakan UTS ini sebagai berikut:<br>
   <li>menambahkan section id data mahasiswa</li>
   <li>mengubah isi tentang saya lagi</li>
   <li>menambahkan dan mengganti section id data mahasiswa yang baru</li>
+  <li>menambahkan session start</li>
 </ol>
--- pertemuan-08/index.php
+++ pertemuan-08/index.php
@@ -92,23 +92,41 @@ endif;
       </form>
     </section>
 
+    <?php
+session_start();
+
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+  $nim            = $_POST["nim"];
+  $nama_lengkap   = $_POST["nama"];
+  $tempat_lahir   = $_POST["tempat_lahir"];
+  $tanggal_lahir  = $_POST["tanggal_lahir"];
+  $hobi           = $_POST["hobi"];
+  $pasangan       = $_POST["pasangan"];
+  $pekerjaan      = $_POST["pekerjaan"];
+  $nama_ortu      = $_POST["nama_ortu"];
+  $nama_kakak     = $_POST["nama_kakak"];
+  $nama_adik      = $_POST["nama_adik"];
+
+  $_SESSION["nim"]           = $nim;
+  $_SESSION["nama_lengkap"]  = $nama_lengkap;
+  $_SESSION["tempat_lahir"]  = $tempat_lahir;
+  $_SESSION["tanggal_lahir"] = $tanggal_lahir;
+  $_SESSION["hobi"]          = $hobi;
+  $_SESSION["pasangan"]      = $pasangan;
+  $_SESSION["pekerjaan"]     = $pekerjaan;
+  $_SESSION["nama_ortu"]     = $nama_ortu;
+  $_SESSION["nama_kakak"]    = $nama_kakak;
+  $_SESSION["nama_adik"]     = $nama_adik;
+
+  header("Location: index.php#about");
+  exit;
+}
+?>
+
+
 
     <section id="about">
       <?php
-            $nim             = "2511500090";
-            $nama_lengkap    = "Akmal Fadhillah Dwiputra";
-            $tempat_lahir    = "Pangkalpinang";
-            $tanggal_lahir   = "29 Oktober 2004";
-            $hobi            = "Push Rank ML🎮";
-            $pasangan        = "Fitriani💞";
-            $pekerjaan       = "Bantu orang tua🍔";
-            $nama_orang_tua  = "Firta Karini👩‍🦰 dan Defiar Fitriansyah👨‍🦰";
-            $nama_kakak      = "Revia Defika👩‍🎓";
-            $nama_adek       = "Nabilla Nasywa dan Rayya Azalea";
-      ?>
-      
-       <section id ="Data Mahasiswa">
-        <?php
             $nim             = "2511500090";
             $nama_lengkap    = "Akmal Fadhillah Dwiputra";
             $tempat_lahir    = "Pangkalpinang";
