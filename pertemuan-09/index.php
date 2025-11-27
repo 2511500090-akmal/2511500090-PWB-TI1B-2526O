5ea32a235344f710757afc0c1970eb59ef5e3998
Akmal Fadhillah Dwiputra <2511500090@mahasiswa.atmaluhur.ac.id>
2025-11-27 11:38:51 +0700

    menambahkan field config

--- pertemuan-09/index.php
+++ pertemuan-09/index.php
@@ -16,55 +16,21 @@ if (isset($_SESSION["sespesan"])):
   $sespesan = $_SESSION["sespesan"];
 endif;
 
-$txtNim = "";
-if (isset($_SESSION["txtNim"])):
-  $txtNim = $_SESSION["txtNim"];
-endif;
+<?php 
+$biodata = $_SESSION["biodata"] ?? [];
 
-$txtNmLengkap = "";
-if (isset($_SESSION["txtNmLengkap"])):
-  $txtNmLengkap = $_SESSION["txtNmLengkap"];
-endif;
-
-$txtT4Lhr = "";
-if (isset($_SESSION["txtT4Lhr"])):
-  $txtT4Lhr = $_SESSION["txtT4Lhr"];
-endif;
-
-$txtTglLhr = "";
-if (isset($_SESSION["txtTglLhr"])):
-  $txtTglLhr = $_SESSION["txtTglLhr"];
-endif;
-
-$txtHobi = "";
-if (isset($_SESSION["txtHobi"])):
-  $txtHobi = $_SESSION["txtHobi"];
-endif;
-
-$txtPasangan = "";
-if (isset($_SESSION["txtPasangan"])):
-  $txtPasangan = $_SESSION["txtPasangan"];
-endif;
-
-$txtKerja = "";
-if (isset($_SESSION["txtKerja"])):
-  $txtKerja = $_SESSION["txtKerja"];
-endif;
-
-$txtNmOrtu = "";
-if (isset($_SESSION["txtNmOrtu"])):
-  $txtNmOrtu = $_SESSION["txtNmOrtu"];
-endif;
-
-$txtNmKakak = "";
-if (isset($_SESSION["txtNmKakak"])):
-  $txtNmKakak = $_SESSION["txtNmKakak"];
-endif;
-
-$txtNmAdik = "";
-if (isset($_SESSION["txtNmAdik"])):
-  $txtNmAdik = $_SESSION["txtNmAdik"];
-endif;
+$fieldconfig = [
+  "nim" => ["label" => "NIM:", "suffix" => ""],
+  "nama" => ["label" => "NamaLengkap:", "suffix" => "🤫"],
+  "tempat" => ["label" => "Tempat:", "suffix" => ""],
+  "tanggal" => ["label" => "Tanggal:", "suffix" => ""],
+  "Hobi" => ["label" => "Hobi:", "suffix" => "🎮"],
+  "pasangan" => ["label" => "Pasangan:", "suffix" => ""],
+  "pekerjaan" => ["label" => "Pekerjaan:", "suffix" => ""],
+  "ortu" => ["label" => "Ortu:", "suffix" => ""],
+  "kakak" => ["label" => "Kakak:", "suffix" => ""],
+  "adik" => ["label" => "Adik:", "suffix" => ""],
+]
 ?>
 
 <!DOCTYPE html>
