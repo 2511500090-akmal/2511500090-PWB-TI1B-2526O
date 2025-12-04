a77003478faad6d634876d159a9d4120fad40f4b
Akmal Fadhillah Dwiputra <2511500090@mahasiswa.atmaluhur.ac.id>
2025-12-04 12:20:31 +0700

    menambahkan kode pada read.php

--- pertemuan-10/read.php
+++ pertemuan-10/read.php
@@ -2,7 +2,7 @@
 require 'koneksi.php'
 
 $sql = "SELECT * FROM tbl_tamu ORDER BY cid DESC";
-$q = mysqli_query($conn, $sql)
+$q = mysqli_query($conn, $sql);
 ?>
 <table border="1" cellpadding="8" cellspacing="0">
     <tr>
